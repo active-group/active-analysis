bae7e8ac7edc3f5b05f43b7296688823f7589da5
Johannes Maier <johannes.maier@active-group.de>
2018-12-17 14:47:33 +0100

    Allow running k-means without neanderthal
    The user needs to pass a function that calculates the centroid
    of a cluster.

--- src/active/analytics/clustering/k_means.clj
+++ src/active/analytics/clustering/k_means.clj
@@ -1,6 +1,14 @@
 (ns active.analytics.clustering.k-means
   (:require [active.analytics.linear-algebra :as lina]))
 
+(defn initial-centroids
+  "Takes `k` random (distinct) elements from `data` to use
+  as initial centroids."
+  [data k]
+  (->> data
+       shuffle
+       (take k)))
+
 (defn nearest-centroid
   "Finds the centroid closest to a point `p`."
   [p centroids distance-fn]
@@ -11,36 +19,47 @@
   nearest centroid."
   [data centroids distance-fn]
   (merge
+   ;; make sure we don't lose any centroids if they don't
+   ;; have any points assigned
    (into {} (map (fn [c] [c nil])
                  centroids))
    (group-by #(nearest-centroid % centroids distance-fn)
              data)))
 
+(defn default-centroid-fn
+  "Calculates a centroid for `points`.
+  For now, assumes the `points` to be neanderthal vectors."
+  [points]
+  (lina/scal (/ 1 (count points))
+             (reduce (fn [acc curr]
+                       (lina/xpy acc curr))
+                     (lina/zero (first points))
+                     points)))
+
 (defn step
   "Calculates the next generation of centroids from
   the current one."
-  [data centroids distance-fn]
+  [data centroids distance-fn centroid-fn]
   (let [clusters (cluster data centroids distance-fn)]
     (map (fn [c]
            (if-some [ps (seq (get clusters c))]
-             (lina/scal (/ 1 (count ps))
-                        (reduce (fn [acc curr]
-                                  (lina/xpy acc curr))
-                                (lina/zero (first ps))
-                                ps))
+             (centroid-fn ps)
              c))
          centroids)))
 
 (defn k-means
   "Performs k-means clustering on a data set, given the
-  desired number of clusters `k`."
+  desired number of clusters `k`.
+  If no `centroid-fn` is given, assumes the data to be
+  neanderthal vectors (for now)."
   ([data distance-fn k threshold max-number-of-iterations]
-   (let [initial-centroids (->> data shuffle (take k))]
-     (k-means data distance-fn k threshold max-number-of-iterations initial-centroids)))
+   (k-means data distance-fn k threshold max-number-of-iterations (initial-centroids data k)))
   ([data distance-fn k threshold max-number-of-iterations initial-centroids]
+   (k-means data distance-fn default-centroid-fn k threshold max-number-of-iterations initial-centroids))
+  ([data distance-fn centroid-fn k threshold max-number-of-iterations initial-centroids]
    (loop [centroids initial-centroids
           i 1]
-     (let [next-centroids (step data centroids distance-fn)]
+     (let [next-centroids (step data centroids distance-fn centroid-fn)]
        (if (or (>= i max-number-of-iterations)
                (every? true?
                        (map (fn [c next-c]
