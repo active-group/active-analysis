afa4df52c586e92358be2ce679bd28dc7f4c8129
Johannes Maier <johannes.maier@active-group.de>
2019-01-09 14:46:52 +0100

    Add PAM BUILD phase implementation
    As an alternative to simple random choice of the initial set
    of medoids, this adds the BUILD part of the PAM (partitioning
    around medoids) algorithm, which greedily chooses a set of
    'better' starting points.

--- src/active_analytics/clustering/k_medoids.clj
+++ src/active_analytics/clustering/k_medoids.clj
@@ -33,13 +33,66 @@
                xs))
      (dissimilarity x x dissimilarities)))
 
-(defn choose-initial-medoids
+(defn choose-initial-medoids-randomly
   "Randomly chooses a set of medoids to start out with."
   [xs k]
   (->> xs
        shuffle
        (take k)
        (into #{})))
+
+(defn first-pam-medoid
+  "Determines the first medoid for the PAM BUILD process."
+  [xs dissimilarities]
+  (apply min-key
+         #(dissimilarity-sum % xs dissimilarities)
+         xs))
+
+(defn min-medoid-distance
+  "Calculates the distance to the closest medoid."
+  [x medoids dissimilarities]
+  (->> medoids
+       (map #(dissimilarity x % dissimilarities))
+       (apply min)))
+
+(defn contribution
+  "Calculates the contribution of `x` to the change in gain
+  when adding `candidate` as additional medoid."
+  [candidate x medoids dissimilarities]
+  (max 0
+       (- (min-medoid-distance x medoids dissimilarities)
+          (dissimilarity candidate x dissimilarities))))
+
+(defn gain
+  "Calculates the gain associated with the choice
+  of a medoid candidate."
+  [candidate xs medoids dissimilarities]
+  (->> xs
+       (remove (partial = candidate))
+       (map #(contribution candidate % medoids dissimilarities))
+       (apply +)))
+
+(defn next-pam-medoid
+  "Determines the best additional medoid, that is,
+  the point with maximal gain w.r.t. the currently
+  chosen medoids."
+  [xs medoids dissimilarities]
+  (apply max-key
+         #(gain % xs medoids dissimilarities)
+         xs))
+
+(defn pam-build
+  "Performs the PAM BUILD initialization. That is,
+  this greedily finds an initial set of medoids by
+  iteratively choosing points which minimize dissimilarity."
+  [xs dissimilarities k]
+  (let [first-medoid (first-pam-medoid xs dissimilarities)]
+    (loop [medoids #{first-medoid}]
+      (if (= (count medoids)
+             k)
+        medoids
+        (recur (conj medoids
+                     (next-pam-medoid xs medoids dissimilarities)))))))
 
 (defn nearest-medoid
   "Finds the medoid that is closest to a point `x`."
@@ -55,7 +108,7 @@
   (group-by #(nearest-medoid % medoids dissimilarities)
             xs))
 
-(defn choose-medoid
+(defn find-medoid
   "Finds the medoid of a set of data points `xs`, that is,
   the point with the lowest sum of dissimilarities."
   [xs dissimilarities]
@@ -68,17 +121,26 @@
   [xs medoids dissimilarities]
   (let [clusters (cluster xs medoids dissimilarities)]
     (into #{}
-          (map #(choose-medoid % dissimilarities)
+          (map #(find-medoid % dissimilarities)
                (vals clusters)))))
 
 (defn k-medoids
   "Performs k-medoid clustering on the data points `xs`,
   given the desired number of clusters `k` and a function
   `distance-fn` that computes the distance between two
-  points."
-  [xs distance-fn k]
-  (let [dissimilarities (dissimilarity-map xs distance-fn)]
-    (loop [medoids (choose-initial-medoids xs k)]
+  points.
+
+  Accepts the keyword argument `:medoid-init-mode` which
+  determines the way the initial set of medoids is created.
+  The default value, `:random`, corresponds to random choice
+  of initial medoids, while `:pam` utilizes the PAM BUILD
+  algorithm to greedily choose `k` 'good' starting medoids."
+  [xs distance-fn k & {:keys [medoid-init-mode] :or {medoid-init-mode :random}}]
+  (let [dissimilarities (dissimilarity-map xs distance-fn)
+        initial-medoids (case medoid-init-mode
+                          :random (choose-initial-medoids-randomly xs k)
+                          :pam (pam-build xs dissimilarities k))]
+    (loop [medoids initial-medoids]
       (let [next-medoids (step xs medoids dissimilarities)]
         (if (= medoids
                next-medoids)
--- test/active_analytics/clustering/k_medoids_test.clj
+++ test/active_analytics/clustering/k_medoids_test.clj
@@ -35,10 +35,10 @@
     (is (= {-1 [-11 -1] 4 [4 5 3]}
            (k-medoids/cluster xs #{-1 4} dissimilarities)))))
 
-(deftest t-choose-medoid)
-(let [dissimilarities (k-medoids/dissimilarity-map [-11 4 5 -1 3] test-util/real-distance)]
-  (is (= 3
-         (k-medoids/choose-medoid [4 5 3 -1] dissimilarities))))
+(deftest t-find-medoid
+  (let [dissimilarities (k-medoids/dissimilarity-map [-11 4 5 -1 3] test-util/real-distance)]
+    (is (= 3
+           (k-medoids/find-medoid [4 5 3 -1] dissimilarities)))))
 
 (deftest t-k-medoids
   (let [xs [-6 11 0 -3 14 13]
@@ -46,4 +46,64 @@
     (is (= #{[-6 0 -3]
              [11 14 13]}
            (into #{}
+                 (:clusters result)))))
+  (let [xs [-6 11 0 -3 14 13]
+        result (k-medoids/k-medoids xs test-util/real-distance 2 :medoid-init-mode :pam)]
+    (is (= #{[-6 0 -3]
+             [11 14 13]}
+           (into #{}
                  (:clusters result))))))
+
+(deftest t-first-pam-medoid
+  (let [xs [-8 -5 0 2 9]
+        dissimilarities (k-medoids/dissimilarity-map xs test-util/real-distance)]
+    (is (= 0
+           (k-medoids/first-pam-medoid xs dissimilarities)))))
+
+(deftest t-min-medoid-distance
+  (let [xs [[1 4] [-3 1] [0 1] [2 0] [5 17] [3 3] [1 1]]
+        dissimilarities (k-medoids/dissimilarity-map xs (test-util/p-distance 1))]
+    (is (= 1.0
+           (k-medoids/min-medoid-distance [1 1]
+                                          [[-3 1] [3 3] [0 1] [2 0]]
+                                          dissimilarities)))))
+
+(deftest t-contribution
+  (let [xs [6 -3 5 0 1 12 -5 2]
+        dissimilarities (k-medoids/dissimilarity-map xs test-util/real-distance)]
+    (is (= 0
+           (k-medoids/contribution -5 2 #{-3 5} dissimilarities)))
+    (is (= 2.0
+           (k-medoids/contribution -5 -3 #{1 2} dissimilarities)))))
+
+(deftest t-gain
+  (let [xs [6 -3 5 0 1 12 -5 2]
+        dissimilarities (k-medoids/dissimilarity-map xs test-util/real-distance)]
+    (is (= 2.0
+           (k-medoids/gain -5 xs #{1} dissimilarities)))
+    (is (= 6.0
+           (k-medoids/gain 2 xs #{0} dissimilarities)))
+    (is (= 6.0
+           (k-medoids/gain 5 xs #{-3 2} dissimilarities)))))
+
+(deftest t-next-pam-medoid
+  (let [xs [6 -3 5 0 1 12 -5 2]
+        dissimilarities (k-medoids/dissimilarity-map xs test-util/real-distance)]
+    (is (= 5
+           (k-medoids/next-pam-medoid xs #{2} dissimilarities))))
+  (let [xs [-5 -2 0 3 10]
+        dissimilarities (k-medoids/dissimilarity-map xs test-util/real-distance)]
+    (is (= 3
+           (k-medoids/next-pam-medoid xs #{-2} dissimilarities)))))
+
+(deftest t-pam-build
+  (let [xs [6 -3 5 0 1 12 -5 2]
+        dissimilarities (k-medoids/dissimilarity-map xs test-util/real-distance)]
+    (is (= #{2}
+           (k-medoids/pam-build xs dissimilarities 1)))
+    (is (= #{2 5}
+           (k-medoids/pam-build xs dissimilarities 2)))
+    (is (= #{2 5 -3}
+           (k-medoids/pam-build xs dissimilarities 3)))
+    (is (= #{2 5 -3 1 6}
+           (k-medoids/pam-build xs dissimilarities 5)))))
